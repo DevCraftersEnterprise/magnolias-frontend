36162c0c29867a44d86637cac5e0c585fadd2f92
Sergio Barreras González <sergiobg.isc@gmail.com>
2026-04-21 10:58:16 -0600

    fix(FloatingDownloadButton): correct email instruction text and update button styles

--- app/components/ui/FloatingDownloadButton.vue
+++ app/components/ui/FloatingDownloadButton.vue
@@ -25,7 +25,7 @@ function downloadPDF() {
               h(
                 "span",
                 { class: "font-medium" },
-                "Después de llenarlo, enviar al siguiente correo:",
+                "Después de llenar el formato, enviar al siguiente correo:",
               ),
               h(
                 "span",
@@ -58,22 +58,27 @@ function downloadPDF() {
 
 <template>
   <button
-    class="fixed bottom-8 right-8 z-50 bg-[#f3b7d1]/80 text-white rounded-full shadow-lg p-4 hover:bg-[#f3b7d1] transition"
+    class="fixed bottom-8 right-8 z-50 bg-red-400 text-black rounded-full shadow-lg p-4 hover:bg-red-500 transition"
     @click="downloadPDF"
     aria-label="Descargar formulario PDF"
   >
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
       fill="none"
       stroke="currentColor"
-      class="h-6 w-6"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      class="lucide lucide-bug-icon lucide-bug"
       viewBox="0 0 24 24"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M4 16v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-2M7 10l5 5 5-5m-5-6v12"
+        d="M12 20v-9m2-4a4 4 0 0 1 4 4v3a6 6 0 0 1-12 0v-3a4 4 0 0 1 4-4zm.12-3.12L16 2"
+      />
+      <path
+        d="M21 21a4 4 0 0 0-3.81-4M21 5a4 4 0 0 1-3.55 3.97M22 13h-4M3 21a4 4 0 0 1 3.81-4M3 5a4 4 0 0 0 3.55 3.97M6 13H2M8 2l1.88 1.88M9 7.13V6a3 3 0 1 1 6 0v1.13"
       />
     </svg>
   </button>
